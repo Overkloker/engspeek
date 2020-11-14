afd7d5628a07809c9d44161847b9b3a3d12f0528
lord <overkloker33@gmail.com>
2020-11-14 20:25:32 +0200

    [main]: layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,37 +4,25 @@ import 'components/Header/styles.scss';
 const Header = () => (
   <div className="header">
     <div className="header-logo">
-      <a href="#" onClick={() => {}}>
-        logo
-      </a>
+      <a href="/#">logo</a>
     </div>
 
     <div className="header-menu-wrapper">
       <ul className="header-menu">
         <li className="header-menu-item">
-          <a href="#" onClick={() => {}}>
-            курсы
-          </a>
+          <a href="/#">курсы</a>
         </li>
         <li className="header-menu-item">
-          <a href="#" onClick={() => {}}>
-            тест на уровень английского
-          </a>
+          <a href="/#">тест на уровень английского</a>
         </li>
         <li className="header-menu-item">
-          <a href="#" onClick={() => {}}>
-            как заговорить
-          </a>
+          <a href="/#">как заговорить</a>
         </li>
         <li className="header-menu-item">
-          <a href="#" onClick={() => {}}>
-            преподаватели
-          </a>
+          <a href="/#">преподаватели</a>
         </li>
         <li className="header-menu-item">
-          <a href="#" onClick={() => {}}>
-            отзывы
-          </a>
+          <a href="/#">отзывы</a>
         </li>
       </ul>
     </div>
