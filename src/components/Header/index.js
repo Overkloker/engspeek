a0a34363e75fc2f37ea826629adaa95e429e5669
lord <overkloker33@gmail.com>
2020-11-14 20:15:11 +0200

    [main]: layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,25 +4,25 @@ import 'components/Header/styles.scss';
 const Header = () => (
   <div className="header">
     <div className="header-logo">
-      <a href="">logo</a>
+      <a href="#">logo</a>
     </div>
 
     <div className="header-menu-wrapper">
       <ul className="header-menu">
         <li className="header-menu-item">
-          <a href="">курсы</a>
+          <a href="#">курсы</a>
         </li>
         <li className="header-menu-item">
-          <a href="">тест на уровень английского</a>
+          <a href="#">тест на уровень английского</a>
         </li>
         <li className="header-menu-item">
-          <a href="">как заговорить</a>
+          <a href="#">как заговорить</a>
         </li>
         <li className="header-menu-item">
-          <a href="">преподаватели</a>
+          <a href="#">преподаватели</a>
         </li>
         <li className="header-menu-item">
-          <a href="">отзывы</a>
+          <a href="#">отзывы</a>
         </li>
       </ul>
     </div>
